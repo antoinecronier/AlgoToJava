92686450eee7d3e825d2a0c181af9fbd676424af
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-07-20 09:14:09 +0200

    Init project with quality

---
+++ src/main/java/imiepoecjava2017/PoecJavaApplication.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package imiepoecjava2017;
+
+/**
+ * @author tactfactory
+ *
+ */
+public class PoecJavaApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yolo");
+	}
+
+}
