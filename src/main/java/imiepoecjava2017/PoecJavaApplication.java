49855699e7ec63a1ec7244292ec509a4a3816d41
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-07-20 09:39:31 +0200

    TP2

--- src/main/java/imiepoecjava2017/PoecJavaApplication.java
+++ src/main/java/imiepoecjava2017/PoecJavaApplication.java
@@ -9,12 +9,30 @@ package imiepoecjava2017;
  */
 public class PoecJavaApplication {
 
+	// ";" instruction de fin de ligne
+
+	// Algo : déclaration variable entière
+	// ENTIER : nomVAr => int nomVar
+
+	// Algo : correspond à la déclaration des variables d'un algorithme
+	// en JAVA doit être noté comme static car utilisé dans une fonction static
+	//	static int jasmine = 7;
+	//	static int aladin = 5;
+	//	static int panierCommun = jasmine + aladin;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Yolo");
+
+		// Algo : alternative à la déclaration static, correspond à une déclaration de variable
+		// dans la fonction
+		int jasmine = 7;
+		int aladin = 5;
+		int panierCommun = jasmine + aladin;
+
+		// Algo : Ecrire
+		System.out.println(panierCommun);
 	}
 
 }
