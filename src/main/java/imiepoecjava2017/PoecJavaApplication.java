90687534450f7bc0568b2e130028076cb244c149
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-07-21 11:25:54 +0200

    base string utils

--- src/main/java/imiepoecjava2017/PoecJavaApplication.java
+++ src/main/java/imiepoecjava2017/PoecJavaApplication.java
@@ -11,159 +11,46 @@ import java.util.Scanner;
  */
 public class PoecJavaApplication {
 
-	public final static int PLAYER_ATTRIBUTS = 5;
-	public final static int PLAYER_PV = 0;
-	public final static int PLAYER_ARMOR = 1;
-	public final static int PLAYER_PA = 2;
-	public final static int PLAYER_WEAPON_COST = 3;
-	public final static int PLAYER_WEAPON_DAMAGE = 4;
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String monString = "";
+		monString = "s;vlsdjlksj dlfj sldk;fl jlsdjlf lkdsj fksdftoto tata";
 
-		// int[][] weapons = new int[5][2];
-		// int[] armors = new int[3];
-		int[][] weapons = { { 3, 4 }, { 2, 1 }, { 10, 6 }, { 1, 1 }, { 6, 3 } };
-		int[] armors = { 1, 2, 4 };
+		if (monString.equals("toto")) {
 
-		int[][] players;
+		}else if (monString.contains("toto")) {
 
-		// Initialisation du scanner
-		Scanner scan = new Scanner(System.in);
+		}else if (monString.endsWith("tata")) {
 
-		players = InitGame(weapons, armors, scan);
+		}else if (monString.startsWith("s;")) {
 
-		// Show game board
-		for (int i = 0; i < players.length; i++) {
-			for (int j = 0; j < PLAYER_ATTRIBUTS; j++) {
-				System.out.print(players[i][j] + " ");
-			}
-			System.out.println();
+		}else if (monString.isEmpty()) {
+
+		}else if (monString == null) {
+
+		}else if (monString.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$")) {
+
+		}else if (monString.length() ==  0) {
+
+		}else if (monString.charAt(0) == 'O') {
+
+		}else if (monString.trim().equals("")) {
+
+		}else if (monString.replace(" - ", "").equals("")) {
+
+		}else if (monString.replaceAll(" - ", "").equals("")) {
+
+		}else if (monString.indexOf("toto") == 1) {
+
+		}else if (monString.split(";")[0].equals("s")) {
+
+		}else if (monString.substring(5, 8).equals("")) {
+
 		}
 
-		PlayGame(players);
-
-		// Fermeture du scanner
-		scan.close();
-	}
-
-	/**
-	 * Play the game
-	 * @param players
-	 */
-	private static void PlayGame(int[][] players) {
-		boolean flag = true;
-		int round = 0;
-
-		// Je continu tant qu'un joueur n'a pas gagné
-		while (flag) {
-			round++;
-			System.out.println("New round : " + round);
-
-			// Les joueurs joue chacun leur tour dans l'ordre
-			for (int i = 0; i < players.length; i++) {
-				System.out.println("Player " + (i + 1) + " turn");
-				int tempPA = players[i][PLAYER_PA] - 1;
-
-				// Je joue si je suis vivant et que je peux tapper
-				while (((tempPA - players[i][PLAYER_WEAPON_COST]) >= 0) && players[i][PLAYER_PV]>0 ) {
-					tempPA -= players[i][PLAYER_WEAPON_COST];
-
-					// Recherche d'un ennemis
-					int ennemi = i;
-					do {
-						ennemi++;
-						if (ennemi >= players.length) {
-							ennemi = 0;
-						}
-					} while (players[ennemi][PLAYER_PV] <= 0 && ennemi != i);
-
-					if (ennemi == i) {
-						System.out.println("End of game, player " + (i + 1)
-								+ " wins.");
-						// Prépare le jeu à être arrêté
-						flag = false;
-						// Force la sortie de la boucle courante
-						break;
-					} else {
-						if (players[i][PLAYER_WEAPON_DAMAGE]
-								- players[ennemi][PLAYER_ARMOR] >= 0) {
-
-							players[ennemi][PLAYER_PV] = players[ennemi][PLAYER_PV]
-									- (-players[ennemi][PLAYER_ARMOR] + players[i][PLAYER_WEAPON_DAMAGE]);
-
-							System.out.println("Player " + (i + 1)
-									+ " hurt player " + (ennemi + 1) + " for "
-									+ players[i][PLAYER_WEAPON_DAMAGE]
-									+ " damages");
-							System.out.println("Player " + (ennemi + 1)
-									+ " have now " + players[ennemi][PLAYER_PV]
-									+ " PV");
-						} else {
-							System.out.println("Player " + (i + 1)
-									+ " is to weak to deal with player "
-									+ (ennemi + 1));
-						}
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * @param weapons
-	 * @param armors
-	 * @param scan
-	 */
-	private static int[][] InitGame(int[][] weapons, int[] armors, Scanner scan) {
-		int nbPlayer;
-		int userSelection;
-		int[][] players;
-
-		// Affichage selection nbJoueur
-		nbPlayer = CallIntMessage(scan, 2, 20,
-				"How many players (min 2/ max 20)?");
-
-		players = new int[nbPlayer][PLAYER_ATTRIBUTS];
-
-		// Affichage des infos pour l'ensemble des joueurs
-		for (int i = 0; i < nbPlayer; i++) {
-			// Affichage selection arme
-			userSelection = CallIntMessage(scan, 1, 5,
-					"Please select a weapon for player " + (i + 1)
-							+ " between theirs:\n"
-							+ "1) Concasseur \n"
-							+ "2) Pelle \n"
-							+ "3) Gatling \n"
-							+ "4) Batte de cricket \n"
-							+ "5) Blaster \n ");
-
-			players[i][PLAYER_WEAPON_COST] = weapons[userSelection - 1][1];
-			players[i][PLAYER_WEAPON_DAMAGE] = weapons[userSelection - 1][0];
-
-			// Affichage selection armure
-			userSelection = CallIntMessage(scan, 1, 3,
-					"Please select a armor for player " + (i + 1)
-							+ " between theirs:\n" + "1) Gilet bleu \n"
-							+ "2) Armure de cuir \n" + "3) Armure de plaque \n");
-
-			players[i][PLAYER_ARMOR] = armors[userSelection - 1];
-
-			// Affichage selection attribut
-			// PV
-			userSelection = CallIntMessage(scan, 1, 100, "Life for player "
-					+ (i + 1));
-			players[i][PLAYER_PV] = userSelection;
-
-			// PA
-			userSelection = CallIntMessage(scan, 1, 100,
-					"Action point for player " + (i + 1));
-			players[i][PLAYER_PA] = userSelection;
-		}
-
-		return players;
+		monString.substring(monString.indexOf("toto"), monString.indexOf("tata")+3).length();
 	}
 
 	/**
