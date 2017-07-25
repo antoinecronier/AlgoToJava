ec47fa8d5b4ca427d4240754e64a74ec506a8e15
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-07-25 17:29:21 +0200

    last error ~

--- src/main/java/imiepoecjava2017/PoecJavaApplication.java
+++ src/main/java/imiepoecjava2017/PoecJavaApplication.java
@@ -4,11 +4,17 @@
 package imiepoecjava2017;
 
 import imiepoecjava2017.utils.asciiart.AsciiArtCommandPrompt;
+import imiepoecjava2017.utils.asciiart.AsciiArtMode;
 
 public class PoecJavaApplication {
 
 	public static void main(String[] args) {
 
-		AsciiArtCommandPrompt.getInstance().print(" !\"#$%&'()*+´-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
+		AsciiArtCommandPrompt.getInstance().print("~");
+		AsciiArtCommandPrompt.getInstance().print("J'aime les frites une fois");
+
+		AsciiArtCommandPrompt.getInstance().changeMode(AsciiArtMode.huge);
+		AsciiArtCommandPrompt.getInstance().printAlhpaMaj("J'aime les frites une fois");
+		AsciiArtCommandPrompt.getInstance().print(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}");
 	}
 }
