eab325a4720e20e7bdc82e23c29f7d0d91b6af87
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-08-08 09:20:48 +0200

    update

--- src/main/java/imiepoecjava2017/PoecJavaApplication.java
+++ src/main/java/imiepoecjava2017/PoecJavaApplication.java
@@ -12,7 +12,7 @@ import imiepoecjava2017.entities.Candidate;
 import imiepoecjava2017.entities.User;
 import imiepoecjava2017.utils.asciiart.AsciiArtCommandPrompt;
 import imiepoecjava2017.utils.asciiart.AsciiArtMode;
-import imiepoecjava2017.utils.file.FromFile;
+import imiepoecjava2017.utils.file.FileManager;
 import imiepoecjava2017.utils.introspection.DumpFields;
 
 public class PoecJavaApplication {
@@ -25,34 +25,44 @@ public class PoecJavaApplication {
 		AsciiArtCommandPrompt.getInstance().changeMode(AsciiArtMode.huge);
 		AsciiArtCommandPrompt.getInstance().printAlhpaMaj("J'aime les frites une fois");
 		AsciiArtCommandPrompt.getInstance().print(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}");
+//
+//		User user = new Candidate();
+//		user.setId(1);
+//		user.setLastname("lastname1");
+//		user.setFirstname("firstname1");
+//		user.setLogin("login1");
+//		user.setPassword("password1");
 
-		User user = new Candidate();
-		user.setId(1);
-		user.setLastname("lastname1");
-		user.setFirstname("firstname1");
-		user.setLogin("login1");
-		user.setPassword("password1");
+//		Map<String,Object>userDump = DumpFields.fielderWithList(user);
+//		for (Entry<String, Object> item : userDump.entrySet()) {
+//			System.out.println(item.getKey() + " " + item.getValue());
+//		}
+//
+//		System.out.println();
+//
+//		ArrayList<String> listField = DumpFields.inspectBaseAttribut(User.class, Object.class);
+//		for (String string : listField) {
+//			AsciiArtCommandPrompt.getInstance().print(string);
+//		}
+//
+//		ArrayList<Method> methods = DumpFields.getSetter(User.class);
+//		for (Method method : methods) {
+//			System.out.println(method.getName());
+//		}
+//
+//		FileManager ff = new FileManager(".\\",".gitignore");
+//		for (String string : ff.getContent()) {
+//			System.out.println(string);
+//		}
+//
+//		FileManager ff1 = new FileManager(".\\test", "test.test");
+//		ArrayList<String> sL = new ArrayList<String>();
+//		sL.add("id0=122 text0=\"mytext\"");
+//		sL.add("id1=230");
+//		ff1.writeToFile(sL);
+//
+//		Map<String,Object> map = ff1.extractFromPattern();
+//		System.out.println(map);
 
-		Map<String,Object>userDump = DumpFields.fielderWithList(user);
-		for (Entry<String, Object> item : userDump.entrySet()) {
-			System.out.println(item.getKey() + " " + item.getValue());
-		}
-
-		System.out.println();
-
-		ArrayList<String> listField = DumpFields.inspectBaseAttribut(User.class, Object.class);
-		for (String string : listField) {
-			AsciiArtCommandPrompt.getInstance().print(string);
-		}
-
-		ArrayList<Method> methods = DumpFields.getSetter(User.class);
-		for (Method method : methods) {
-			System.out.println(method.getName());
-		}
-
-		FromFile ff = new FromFile("C:\\Users\\tactfactory\\workspaceLuna\\IMIE-POEC-JAVA-2017\\.gitignore");
-		for (String string : ff.getList()) {
-			System.out.println(string);
-		}
 	}
 }
