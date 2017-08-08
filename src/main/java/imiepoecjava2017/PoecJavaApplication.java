2d9f22bf370d4cf60ca49ad1b4de2b651d398226
Antoine Cronier <antoine.cronier@tactfactory.com>
2017-08-08 13:41:36 +0200

    object

--- src/main/java/imiepoecjava2017/PoecJavaApplication.java
+++ src/main/java/imiepoecjava2017/PoecJavaApplication.java
@@ -3,9 +3,21 @@
  */
 package imiepoecjava2017;
 
+import imiepoecjava2017.entities.Role;
+import imiepoecjava2017.entities.User;
+
 public class PoecJavaApplication {
 
 	public static void main(String[] args) {
+		//Instanciate a new user
+		User monUser = new User();
+		monUser.setFirstname("jean-loui");
+		monUser.getRoles().add(new Role(1,"role1"));
+		monUser.getRoles().add(new Role(2,"role2"));
+		System.out.println(monUser.getFirstname());
+		System.out.println(monUser.toString());
 
+		User monUser1 = new User();
+		System.out.println(monUser1.toString());
 	}
 }
